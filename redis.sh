9896f81811e03d7bb32bab20a06d24d333d464b0
DharmarajuG <dharmarajurps@gmail.com>
2025-03-07 07:11:56 +0530

    This is ansible file

---
+++ redis.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#AMI=ami-0b4f379183e5706b9 #this keeps on changing
+AMI=ami-09c813fb71547fc4f
+SG_ID=sg-008ec3ba4d4402c28 #replace with your SG ID
+INSTANCES=("redis")
+ZONE_ID=Z06421211ES3IRIHBMUE0 # replace your zone ID
+DOMAIN_NAME="gdharma.xyz"
+
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-008ec3ba4d4402c28 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
+done
